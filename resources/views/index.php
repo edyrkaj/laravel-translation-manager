ea77beb5d7a040e027b0fa345be304f6e540519a
Eledi Dyrkaj <edyrkaj@gmail.com>
2017-11-08 09:38:35 +0100

    Added some optimisation of template

--- resources/views/index.php
+++ resources/views/index.php
@@ -16,11 +16,21 @@
         a.status-1{
             font-weight: bold;
         }
+
+        .editable-input textarea {width: 700px!important;}
     </style>
+    
+    <?php
+    $group = isset($group) ? $group : "";
+    $groups = isset($groups) ? $groups : [];
+    ?>
     <script>
         jQuery(document).ready(function($){
 
             $.ajaxSetup({
+                headers: {
+                    'X-CSRF-TOKEN': '<?= csrf_token() ?>'
+                },
                 beforeSend: function(xhr, settings) {
                     console.log('beforesend');
                     settings.data += "&_token=<?= csrf_token() ?>";
@@ -50,13 +60,13 @@
             });
 
             $("a.delete-key").click(function(event){
-              event.preventDefault();
-              var row = $(this).closest('tr');
-              var url = $(this).attr('href');
-              var id = row.attr('id');
-              $.post( url, {id: id}, function(){
-                  row.remove();
-              } );
+                event.preventDefault();
+                var row = $(this).closest('tr');
+                var url = $(this).attr('href');
+                var id = row.attr('id');
+                $.post( url, {id: id}, function(){
+                    row.remove();
+                } );
             });
 
             $('.form-import').on('ajax:success', function (e, data) {
@@ -78,8 +88,9 @@
 </head>
 <body>
 <div style="width: 80%; margin: auto;">
-    <h1>Translation Manager</h1>
-    <p>Warning, translations are not visible until they are exported back to the app/lang file, using 'php artisan translation:export' command or publish button.</p>
+    <h1>Ezcelip - Translation Manager</h1>
+    <p>Warning, translations are not visible until they are exported back to the app/lang file, using 'php artisan translation:export' command or <b>publish</b> button.</p>
+    <p style="color: darkred;">Please do not change <b>:parameter</b> or <b>{{parameter}}</b> values on text content</p>
     <div class="alert alert-success success-import" style="display:none;">
         <p>Done importing, processed <strong class="counter">N</strong> items! Reload this page to refresh the groups!</p>
     </div>
@@ -96,27 +107,27 @@
     <?php endif; ?>
     <p>
         <?php if(!isset($group)) : ?>
-        <form class="form-inline form-import" method="POST" action="<?= action('\Edyrkaj\TranslationManager\Controller@postImport') ?>" data-remote="true" role="form">
+    <form class="form-inline form-import" method="POST" action="<?= action('\Edyrkaj\TranslationManager\Controller@postImport') ?>" data-remote="true" role="form">
+        <input type="hidden" name="_token" value="<?php echo csrf_token(); ?>">
+        <select name="replace" class="form-control">
+            <option value="0">Append new translations</option>
+            <option value="1">Replace existing translations</option>
+        </select>
+        <button type="submit" class="btn btn-success"  data-disable-with="Loading..">Import groups</button>
+    </form>
+    <form class="form-inline form-find" method="POST" action="<?= action('\Edyrkaj\TranslationManager\Controller@postFind') ?>" data-remote="true" role="form" data-confirm="Are you sure you want to scan you app folder? All found translation keys will be added to the database.">
+        <input type="hidden" name="_token" value="<?php echo csrf_token(); ?>">
+        <p></p>
+        <button type="submit" class="btn btn-info" data-disable-with="Searching.." >Find translations in files</button>
+    </form>
+<?php endif; ?>
+    <?php if(isset($group)) : ?>
+        <form class="form-inline form-publish" method="POST" action="<?= action('\Edyrkaj\TranslationManager\Controller@postPublish', $group) ?>" data-remote="true" role="form" data-confirm="Are you sure you want to publish the translations group '<?= $group ?>? This will overwrite existing language files.">
             <input type="hidden" name="_token" value="<?php echo csrf_token(); ?>">
-            <select name="replace" class="form-control">
-                <option value="0">Append new translations</option>
-                <option value="1">Replace existing translations</option>
-            </select>
-            <button type="submit" class="btn btn-success"  data-disable-with="Loading..">Import groups</button>
+            <button type="submit" class="btn btn-info" data-disable-with="Publishing.." >Publish translations</button>
+            <a href="<?= action('\Edyrkaj\TranslationManager\Controller@getIndex') ?>" class="btn btn-default">Back</a>
         </form>
-        <form class="form-inline form-find" method="POST" action="<?= action('\Edyrkaj\TranslationManager\Controller@postFind') ?>" data-remote="true" role="form" data-confirm="Are you sure you want to scan you app folder? All found translation keys will be added to the database.">
-            <input type="hidden" name="_token" value="<?php echo csrf_token(); ?>">
-            <p></p>
-            <button type="submit" class="btn btn-info" data-disable-with="Searching.." >Find translations in files</button>
-        </form>
-        <?php endif; ?>
-        <?php if(isset($group)) : ?>
-            <form class="form-inline form-publish" method="POST" action="<?= action('\Edyrkaj\TranslationManager\Controller@postPublish', $group) ?>" data-remote="true" role="form" data-confirm="Are you sure you want to publish the translations group '<?= $group ?>? This will overwrite existing language files.">
-                <input type="hidden" name="_token" value="<?php echo csrf_token(); ?>">
-                <button type="submit" class="btn btn-info" data-disable-with="Publishing.." >Publish translations</button>
-                <a href="<?= action('\Edyrkaj\TranslationManager\Controller@getIndex') ?>" class="btn btn-default">Back</a>
-            </form>
-        <?php endif; ?>
+    <?php endif; ?>
     </p>
     <form role="form">
         <input type="hidden" name="_token" value="<?php echo csrf_token(); ?>">
@@ -136,44 +147,44 @@
             <input type="submit" value="Add keys" class="btn btn-primary">
         </form>
         <hr>
-    <h4>Total: <?= $numTranslations ?>, changed: <?= $numChanged ?></h4>
-    <table class="table">
-        <thead>
-        <tr>
-            <th width="15%">Key</th>
-            <?php foreach($locales as $locale): ?>
-                <th><?= $locale ?></th>
-            <?php endforeach; ?>
-            <?php if($deleteEnabled): ?>
-                <th>&nbsp;</th>
-            <?php endif; ?>
-        </tr>
-        </thead>
-        <tbody>
-
-        <?php foreach($translations as $key => $translation): ?>
-            <tr id="<?= $key ?>">
-                <td><?= $key ?></td>
+        <h4>Total: <?= $numTranslations ?>, changed: <?= $numChanged ?></h4>
+        <table class="table">
+            <thead>
+            <tr>
+                <th width="15%">Key</th>
                 <?php foreach($locales as $locale): ?>
-                    <?php $t = isset($translation[$locale]) ? $translation[$locale] : null?>
-
-                    <td>
-                        <a href="#edit" class="editable status-<?= $t ? $t->status : 0 ?> locale-<?= $locale ?>" data-locale="<?= $locale ?>" data-name="<?= $locale . "|" . $key ?>" id="username" data-type="textarea" data-pk="<?= $t ? $t->id : 0 ?>" data-url="<?= $editUrl ?>" data-title="Enter translation"><?= $t ? htmlentities($t->value, ENT_QUOTES, 'UTF-8', false) : '' ?></a>
-                    </td>
+                    <th><?= $locale ?></th>
                 <?php endforeach; ?>
                 <?php if($deleteEnabled): ?>
-                    <td>
-                        <a href="<?= action('\Edyrkaj\TranslationManager\Controller@postDelete', [$group, $key]) ?>" class="delete-key" data-confirm="Are you sure you want to delete the translations for '<?= $key ?>?"><span class="glyphicon glyphicon-trash"></span></a>
-                    </td>
+                    <th>&nbsp;</th>
                 <?php endif; ?>
             </tr>
-        <?php endforeach; ?>
+            </thead>
+            <tbody>
+            
+            <?php foreach($translations as $key => $translation): ?>
+                <tr id="<?= $key ?>">
+                    <td><?= $key ?></td>
+                    <?php foreach($locales as $locale): ?>
+                        <?php $t = isset($translation[$locale]) ? $translation[$locale] : null?>
 
-        </tbody>
-    </table>
+                        <td>
+                            <a href="#edit" class="editable status-<?= $t ? $t->status : 0 ?> locale-<?= $locale ?>" data-locale="<?= $locale ?>" data-name="<?= $locale . "|" . $key ?>" id="username" data-type="textarea" data-pk="<?= $t ? $t->id : 0 ?>" data-url="<?= $editUrl ?>" data-title="Enter translation"><?= $t ? htmlentities($t->value, ENT_QUOTES, 'UTF-8', false) : '' ?></a>
+                        </td>
+                    <?php endforeach; ?>
+                    <?php if($deleteEnabled): ?>
+                        <td>
+                            <a href="<?= action('\Edyrkaj\TranslationManager\Controller@postDelete', [$group, $key]) ?>" class="delete-key" data-confirm="Are you sure you want to delete the translations for '<?= $key ?>?"><span class="glyphicon glyphicon-trash"></span></a>
+                        </td>
+                    <?php endif; ?>
+                </tr>
+            <?php endforeach; ?>
+
+            </tbody>
+        </table>
     <?php else: ?>
         <p>Choose a group to display the group translations. If no groups are visible, make sure you have run the migrations and imported the translations.</p>
-
+    
     <?php endif; ?>
 </div>
 
